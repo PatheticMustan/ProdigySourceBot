168a3aeba2026e6933bd6e099f754190112a5795
Mustan Pathetic <patheticmustan@gmail.com>
2020-10-07 20:43:25 -0400

    v0.2!!!!!
    now it works!!!!

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
-const { Octokit } = require("@octokit/core");
+const { Octokit } = require("@octokit/rest");
+const fs = require("fs");
 
 const config = require("./config.json");
-const octokit = new Octokit({ auth: config.githubPersonalAccessToken });
+const octokit = new Octokit({
+    auth: config.githubPersonalAccessToken
+});
 
-const fileUpload = async() => {
-    try {
-        const contentEncoded = fs.readFileSync("./test.txt", "utf-8").toString("base64");
+const fileUpload = async () => {
+    const fileContent = fs.readFileSync("./test.txt", "utf-8");
 
-        const { data } = await octokit.repos.createOrUpdateFileContents({
-            owner: "PatheticMustan",
-            repo: "ProdigySource",
-            path: "OUTPUT.md",
-            message: "feat: Added OUTPUT.md programatically",
-            content: contentEncoded,
-            committer: {
-                name: `Octokit Bot`,
-                email: "your-email",
-            },
-            author: {
-                name: "Octokit Bot",
-                email: "your-email",
-            },
-        });
+    const contentEncoded = Buffer.from(fileContent).toString("base64");
+    console.log(contentEncoded);
 
-        console.log(data);
-    } catch (err) {
-        console.error(err);
-    }
-
-    await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    const { data } = await octokit.repos.createOrUpdateFileContents({
         owner: "PatheticMustan",
         repo: "ProdigySource",
+
         path: "test.txt",
-        message: "git commit message thingy",
-        content: "somebody once told me the world was gonna roll me"
+        message: "test",
+        content: contentEncoded,
+        
+        committer: config.githubInfo,
+        author: config.githubInfo
     })
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 }
 
 fileUpload();
