c9237f4fecc0382785d22d6b1a493f4baa1bb65d
Mustan Pathetic <patheticmustan@gmail.com>
2020-10-07 21:08:35 -0400

    brain just expanding

--- index.js
+++ index.js
@@ -6,25 +6,39 @@ const octokit = new Octokit({
     auth: config.githubPersonalAccessToken
 });
 
+`https://api.github.com/repos/${config.githubRepoInfo.owner}/${config.githubRepoInfo.repo}/contents/test.txt`
+
 const fileUpload = async () => {
     const fileContent = fs.readFileSync("./test.txt", "utf-8");
 
     const contentEncoded = Buffer.from(fileContent).toString("base64");
-    console.log(contentEncoded);
 
-    const { data } = await octokit.repos.createOrUpdateFileContents({
-        owner: "PatheticMustan",
-        repo: "ProdigySource",
+    const fileSha = await octokit.repos.getContent({
+        owner: config.githubRepoInfo.owner,
+        repo: config.githubRepoInfo.repo,
+        path: "deletelater.txt",
+    })
+    .then(res => res.data.sha)
+    .catch(err => console.error(err));
 
-        path: "test.txt",
-        message: "test",
-        content: contentEncoded,
-        
+    console.log("eeeeee: " + fileSha);
+
+    /*const commitData = await octokit.repos.createOrUpdateFileContents({
+        owner: config.githubRepoInfo.owner,
+        repo: config.githubRepoInfo.repo,
         committer: config.githubInfo,
-        author: config.githubInfo
+        author: config.githubInfo,
+
+        sha: "",
+
+        path: "deletelater.txt",
+        message: "test v2",
+        content: contentEncoded
     })
     .then(res => console.log(res))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err));*/
+
+    
 }
 
 fileUpload();
