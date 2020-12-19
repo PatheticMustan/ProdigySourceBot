a0eb9b125f6174c251582fda1f387779d7889167
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-18 20:46:26 -0500

    ?????????????????????? dev release

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const getProdigyEducationStatus = async () => {
     return await (await fetch("https://api.prodigygame.com/education-api/status")).json();
 }
 
-const githubFileUpload = async (fileName, content) => {
+const githubFileUpload = async (fileName, content, commitMessage) => {
     const contentEncoded = Buffer.from(content).toString("base64");
 
     console.log(`Looking up ${config.github.repoInfo.owner}/${config.github.repoInfo.repo}/${fileName} on Github...`)
@@ -36,21 +36,24 @@ const githubFileUpload = async (fileName, content) => {
         console.log("File doesn't exist!")
     }
 
+    const cm = commitMessage || `Update ${fileName}`;
+
     const commitData = await octokit.repos.createOrUpdateFileContents({
-        owner: config.githubRepoInfo.owner,
-        repo: config.githubRepoInfo.repo,
-        committer: config.githubInfo,
-        author: config.githubInfo,
+        owner: config.github.repoInfo.owner,
+        repo: config.github.repoInfo.repo,
+        committer: config.github.authorInfo,
+        author: config.github.authorInfo,
 
         // If the sha exists, put it in! Otherwise just meh
         sha: (fileData?.data?.sha ? fileData.data.sha : ""),
 
         path: fileName,
-        message: `Update ${fileName}`,
+        message: cm,
         content: contentEncoded
     })
-    .then(res => console.log(res))
     .catch(err => console.error(err));
+
+    console.log(commitMessage);
 }
 
 const main = async () => {
@@ -68,14 +71,12 @@ const main = async () => {
 
     const createEmbedObject = (name, color, lastValue, newValue) => {
         return {
-            "embeds": [{
                 "title": `New Prodigy ${name}`,
                 "color": color,
                 "fields": [
                     { "name": `Last ${name}`, "value": lastValue, "inline": true },
                     { "name": `New ${name}`, "value": newValue, "inline": true }
                 ]
-            }]
         };
     }
 
@@ -97,32 +98,32 @@ const main = async () => {
 
         const embeds = [];
         
-        if (last.version !== version) {
+        if (true || last.version !== version) {
             // 11343081 #AD14E9, no matter where I go, I see her name
             embeds.push(createEmbedObject("Version", 11343081, last.version, version));
 
-            console.log(`Prodigy updated to version ${version}.`);
-            const newCode = (await (await fetch("https://code.prodigygame.com/code/${version}/game.min.js")).text())
+            console.log("starting new run");
+            const newCode = (await (await fetch(`https://code.prodigygame.com/code/${version}/game.min.js`)).text())
             const beautified = js(newCode);
 
-            githubFileUpload(`${version}.js`, beautified);
+            githubFileUpload(`${version}.js`, beautified, `Updated from ${last.version} to ${version}`);
             
-            // add twitter bot
+            // TODO: add twitter bot
         }
 
-        if (last.build !== build) {
+        if (true || last.build !== build) {
             // 1370388 #14e914
             embeds.push(createEmbedObject("Build", 1370388, last.build, build));
         }
 
-        if (last.educationDataVersion !== educationDataVersion) {
+        if (true || last.educationDataVersion !== educationDataVersion) {
             // 15300372 #e97714
-            embeds.push(createEmbedObject("Education Data Version", 15300372, last.educationDataVersion, educationDataVersion));
+            embeds.push(createEmbedObject("Education Data", 15300372, last.educationDataVersion, educationDataVersion));
         }
 
-        if (last.educationFrontendVersion !== educationFrontendVersion) {
+        if (true || last.educationFrontendVersion !== educationFrontendVersion) {
             // 1366249 #14d8e9
-            embeds.push(createEmbedObject("Education Frontend Version", 1366249, last.educationFrontendVersion, educationFrontendVersion));
+            embeds.push(createEmbedObject("Education Frontend", 1366249, last.educationFrontendVersion, educationFrontendVersion));
         }
 
         // if embeds > 0...
@@ -131,8 +132,7 @@ const main = async () => {
                 embeds: embeds
             });
         }
-    }, 10 * 60 * 1000);
+    }, 20 * 1000);
 }
 
-// main();
-console.log(js("function add(a,b,c,          d, e,f,g){return a+b+c+d+e + f + g}"))
+main();
